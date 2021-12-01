893b8c7c308c642eda4f9d106e03b799b9136847
DudlyDude <dudlydude@live.com>
2021-12-01 00:19:10 -0500

    Update to use postgresql

--- src/vendure-config.ts
+++ src/vendure-config.ts
@@ -4,7 +4,7 @@ import {
     DefaultSearchPlugin,
     VendureConfig,
 } from '@vendure/core'; 
-//import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
+import { defaultEmailHandlers, EmailPlugin } from '@vendure/email-plugin';
 import { AssetServerPlugin } from '@vendure/asset-server-plugin';
 import { AdminUiPlugin } from '@vendure/admin-ui-plugin';
 import path from 'path';
@@ -19,36 +19,27 @@ export const config: VendureConfig = {
     apiOptions: {
         port: procPORT,
         adminApiPath: 'admin-api',
-        adminApiPlayground: {
-            settings: {
-                'request.credentials': 'include',
-            } as any,
-        },// turn this off for production
-        adminApiDebug: true, // turn this off for production
         shopApiPath: 'shop-api',
-        shopApiPlayground: {
-            settings: {
-                'request.credentials': 'include',
-            } as any,
-        },// turn this off for production
-        shopApiDebug: true,// turn this off for production
     },
     authOptions: {
         superadminCredentials: {
             identifier: 'supercanivibe',
             password: '@dmin42069',
         },
-        requireVerification: false,
         tokenMethod: 'cookie',
         cookieOptions: {
           secret: process.env.COOKIE_SECRET || 'cookie-secret',
         },
     },
     dbConnectionOptions: {
-        type: 'better-sqlite3',
-        synchronize: true, // turn this off for production
+        type: 'postgres',
+        synchronize: false, // turn this off for production
         logging: false,
-        database: path.join(__dirname, '../vendure.sqlite'),
+        database: 'postgresql-metric-37123',
+        host: 'process.env.DATABASE_URL',
+        port: 5432,
+        username: '',
+        password: '',
         migrations: [path.join(__dirname, '../migrations/*.ts')],
     },
     paymentOptions: {
@@ -62,20 +53,26 @@ export const config: VendureConfig = {
         }),
         DefaultJobQueuePlugin.init({ useDatabaseForBuffer: true }),
         DefaultSearchPlugin.init({ bufferUpdates: false, indexStockStatus: true }),
-        // EmailPlugin.init({
-        //     devMode: true,
-        //     outputPath: path.join(__dirname, '../static/email/test-emails'),
-        //     route: 'mailbox',
-        //     handlers: defaultEmailHandlers,
-        //     templatePath: path.join(__dirname, '../static/email/templates'),
-        //     globalTemplateVars: {
-        //         // The following variables will change depending on your storefront implementation
-        //         fromAddress: '"example" <noreply@example.com>',
-        //         verifyEmailAddressUrl: 'http://localhost:8080/verify',
-        //         passwordResetUrl: 'http://localhost:8080/password-reset',
-        //         changeEmailAddressUrl: 'http://localhost:8080/verify-email-address-change'
-        //     },
-        // }),
+        EmailPlugin.init({
+            transport: {
+                type: 'smtp',
+                host: '',
+                port: 666,
+                auth: {
+                  user: '',
+                  pass: '',
+                }
+              },
+            handlers: defaultEmailHandlers,
+            templatePath: path.join(__dirname, '../static/email/templates'),
+            globalTemplateVars: {
+                // The following variables will change depending on your storefront implementation
+                fromAddress: '',
+                verifyEmailAddressUrl: '',
+                passwordResetUrl: '',
+                changeEmailAddressUrl: ''
+            },
+        }),
         AdminUiPlugin.init({
             route: 'admin',
             port: procadminPORT,
